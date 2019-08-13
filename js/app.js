efb524eb45e030d78018d7e174d394e39cd43c09
breelong7 <breelong7@gmail.com>
2019-08-13 16:20:22 -0700

    added initial alert when page loads and two variables to store prompt answers

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+alert('Gasp! You have somehow stumbled upon an ancient webpage where the most powerful genie lies in wait! Answer 5 yes or no questions about Breenie the Genie, and she will grant any wish you can dream of. Good luck!');
+
+var dogsRule = prompt('Does Breenie prefer dogs over cats?');
+
+if(dogsRule ==='yes'){
+  alert('Correct! That was an easy one. All powerful genies prefer dogs.');
+} else{
+  alert('Incorrect! genies love dogs. Since Breenie is a merciful genie, she will allow you to continue');
+}
+
+var isPsychic = prompt('Can Breenie see the future?');
+
+if(isPsychic === 'yes'){
+  alert('Correct! Congrats to you for graduating Code 201 in October!');
+} else{
+  alert('That is not correct. Since Breenie has been stuck in the lamp for thousands of years with nobody to talk to, she will give you another chance.');
+}
+
+var 
