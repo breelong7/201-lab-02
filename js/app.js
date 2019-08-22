6e5cd113b5f2022334239ffd2176751586bfb008
breelong7 <breelong7@gmail.com>
2019-08-21 20:07:25 -0700

    wrapped my loops in functions

--- js/app.js
+++ js/app.js
@@ -11,70 +11,81 @@ var promptResponse = [];
 var correctAnswerCount = 0;
 
 //Loop through all questions in myQuestions[]
-for(var i = 0; i < myQuestions.length; i++){
-  //Prompt user with myQuestions questions
-  // var promptResponse = prompt(myQuestions[i]);
-  promptResponse[i] = prompt(myQuestions[i]);
-  promptResponse[i] = promptResponse[i].toLowerCase();
-  console.log('User answer ' + promptResponse[i]);
+function fiveQuestions(){
+  for(var i = 0; i < myQuestions.length; i++){
+    //Prompt user with myQuestions questions
+    // var promptResponse = prompt(myQuestions[i]);
+    promptResponse[i] = prompt(myQuestions[i]);
+    promptResponse[i] = promptResponse[i].toLowerCase();
+    console.log('User answer ' + promptResponse[i]);
 
-  if(promptResponse[i] === myAnswersA[i] || promptResponse[i] === myAnswersB[i]){
-    alert(userName + ', you got it!');
-    correctAnswerCount++;
+    if(promptResponse[i] === myAnswersA[i] || promptResponse[i] === myAnswersB[i]){
+      alert(userName + ', you got it!');
+      correctAnswerCount++;
 
-  } else {
-    alert('That is incorrect');
+    } else {
+      alert('That is incorrect');
+    }
+    console.log('Correct answer count ' + correctAnswerCount);
   }
-  console.log('Correct answer count ' + correctAnswerCount);
 }
+
+fiveQuestions();
 
 //Favorite number
-for(var q = 0; q < 4; q++) {
-  var favNumber = prompt('What is my favorite number?');
-  console.log('What is Bree\'s favorite number?' + favNumber);
-  favNumber = parseInt(favNumber);
-  if(favNumber === 13) {
-    alert('Correct! ');
-    correctAnswerCount++;
-    break;
-  } else if(favNumber < 13) {
-    alert('Wrong! Too low');
-  } else if (favNumber > 13) {
-    alert('Incorrect! Too high');
+function favNumber(){
+  for(var q = 0; q < 4; q++) {
+    var favNumber = prompt('What is my favorite number?');
+    console.log('What is Bree\'s favorite number?' + favNumber);
+    favNumber = parseInt(favNumber);
+    if(favNumber === 13) {
+      alert('Correct! ');
+      correctAnswerCount++;
+      break;
+    } else if(favNumber < 13) {
+      alert('Wrong! Too low');
+    } else if (favNumber > 13) {
+      alert('Incorrect! Too high');
+    }
   }
 }
+favNumber();
 
-// var countSports = 0;
+
 var sports = ['kickball', 'softball', 'volleyball', 'dodgeball'];
 
 alert('Okay, here\'s the real test. I\'ll give you 6 attempts to guess at least one sport I have played. Ready, go!');
 
+function guessSix() {
 //Up to 6 attempts
-for(var r = 0; r < 6; r++) {
-  //prompt, normalize, compare
-  //alert
-  var sportGuess = prompt('What sport do you think I\'ve played?');
-  console.log('guess sports value - ' + sportGuess);
-  sportGuess = sportGuess.toLowerCase();
-  var isCorrect = false;
-  //For each sport in the sport array
-  //s starts at 0 and represents the first index in the sports array
-  for(var s = 0; s < sports.length; s++){
-    console.log('attempt ' + (s+1));
-    console.log('Correct answer: ' + sports[s]);
-    if(sportGuess === sports[s]){
-      isCorrect = true;
+  for(var r = 0; r < 6; r++) {
+    //prompt, normalize, compare
+    //alert
+    var sportGuess = prompt('What sport do you think I\'ve played?');
+    console.log('guess sports value - ' + sportGuess);
+    sportGuess = sportGuess.toLowerCase();
+    var isCorrect = false;
+    //For each sport in the sport array
+    //s starts at 0 and represents the first index in the sports array
+    for(var s = 0; s < sports.length; s++){
+      console.log('attempt ' + (s+1));
+      console.log('Correct answer: ' + sports[s]);
+      if(sportGuess === sports[s]){
+        isCorrect = true;
+      }
     }
+    if(isCorrect) {
+      alert('That\'s correct! I have played ' + sports[0] + ', ' + sports[1] + ', ' + sports[2] + ', and ' + sports[3]);
+      correctAnswerCount++;
+      break;
+    } else {
+      alert('Try again!');
+    }
+  } if (s > sports.length){
+    alert('Sorry! You did not guess correctly. I have played ' + sports[0] + ', ' + sports[1] + ', ' + sports[2] + ', and ' + sports[3]);
   }
-  if(isCorrect) {
-    alert('That\'s correct! I have played ' + sports[0] + ', ' + sports[1] + ', ' + sports[2] + ', and ' + sports[3]);
-    correctAnswerCount++;
-    break;
-  } else {
-    alert('Try again!');
-  }
-} if (s > sports.length){
-  alert('Sorry! You did not guess correctly. I have played ' + sports[0] + ', ' + sports[1] + ', ' + sports[2] + ', and ' + sports[3]);
 }
+
+guessSix();
 
 alert('Your score is ' + correctAnswerCount + ' out of 7.');
